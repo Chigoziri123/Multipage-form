3a3518ab50315efa50eb7de362962922fccb3ba0
Chigoziri123 <anuna.s@bullseyemanpower.com>
2024-02-07 20:20:43 +0100

    Code adjusted

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@
 // import SectiononeForm from "./components/SectiononeForm";
 // import SectiontwoForm from "./components/SectiontwoForm";
 // import RegistrationForm from "./components/RegistrationForm";
-
+import {useState} from "react"
 import Stepper from "./components/Stepper";
 import StepperControl from "./components/StepperControl";
+import Personal from "./components/steps/Personal";
+import Education from "./components/steps/Education";
+import CourseChoice from "./components/steps/CourseChoice";
+import Others from "./components/steps/Others";
+import Final from "./components/steps/Final";
+
 
 // function App() {
 //   return (
@@ -23,12 +29,43 @@ import StepperControl from "./components/StepperControl";
 
 
 function App() {
+
+  const [currentStep, setCuurrentStep] = useState(1);
+
+  const steps = [
+    "Personal Information",
+    "Educational Qualifications",
+    "Course Choice",
+    "Document Upload",
+    "Other Information",
+    "Complete"
+  ]
+
+  const displayStep =(step) => {
+    switch(step) {
+      case 1:
+        return <Personal />
+      case 2:
+        return <Education />
+      case 3:
+        return <CourseChoice />
+      case 4:
+        return <Others />
+      case 5:
+        return <Final />
+      // case 6:
+      //   return <Personal />
+  }
+  }
   return (
     <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
       
       {/* Stepper */}
       <div className="container horizontal mt-5"> 
-      <Stepper />
+        <Stepper 
+          steps={steps}
+          currentStep={currentStep}
+        />
       </div>
      
 
---
+++ src/components/steps/CourseChoice.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CourseChoice = () => {
+  return (
+    <div>CourseChoice</div>
+  )
+}
+
+export default CourseChoice
